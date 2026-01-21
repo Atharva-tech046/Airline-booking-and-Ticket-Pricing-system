56c7ac2de3c90c56ca5846edb5388cf154cd900b
Atharva Mohite <atharvam2017@gmail.com>
2026-01-21 23:13:04 +0530

    Switch to SQLite for easy deployment

--- config/settings.py
+++ config/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'airline_db',
-        'USER': 'postgres',             # <--- Likely 'postgres'
-        'PASSWORD': 'Atharva@31', # <--- YOUR real password here
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
